b285d9446f580275113220a44ffe1034fba7bc48
saulotarsobc <saulotarsobc@gmail.com>
2024-02-26 12:16:31 -0300

    :fire:get total count Tags table

--- py/src/main.py
+++ py/src/main.py
@@ -14,20 +14,21 @@ with zipfile.ZipFile(jwfile1, 'r') as zip_ref:
 
     connection = sqlite3.connect(uploadedDb)
     cursor = connection.cursor()
-    cursor.execute("SELECT Count(*) FROM Tag")
-    totoalTags = cursor.fetchall()[0][0]
-    print(f"Total de tags: {totoalTags}")
+    cursor.execute("SELECT * FROM Tag")
+    Tags = cursor.fetchall()[0][0]
+    print(Tags)
 
-# descompactar bkp2
-with zipfile.ZipFile(jwfile2, 'r') as zip_ref:
-    files = zip_ref.namelist()
-    zip_ref.extractall("./data-2")
 
-    # uploadedDb = "./data-2/{0}".format([zipname for zipname in files if zipname.endswith(".db")][0])
-    uploadedDb = "./data-2/userData.db"
+# # descompactar bkp2
+# with zipfile.ZipFile(jwfile2, 'r') as zip_ref:
+#     files = zip_ref.namelist()
+#     zip_ref.extractall("./data-2")
 
-    connection = sqlite3.connect(uploadedDb)
-    cursor = connection.cursor()
-    cursor.execute("SELECT Count(*) FROM Tag")
-    totoalTags = cursor.fetchall()[0][0]
-    print(f"Total de tags: {totoalTags}")
+#     # uploadedDb = "./data-2/{0}".format([zipname for zipname in files if zipname.endswith(".db")][0])
+#     uploadedDb = "./data-2/userData.db"
+
+#     connection = sqlite3.connect(uploadedDb)
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT Count(*) FROM Tag")
+#     totoalTags = cursor.fetchall()[0][0]
+#     print(f"Total de tags: {totoalTags}")
