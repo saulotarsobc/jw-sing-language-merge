ebe9c588ef8e84670b2b38bd8ab40f948e4a20c5
saulotarsobc <saulotarsobc@gmail.com>
2024-02-26 13:34:42 -0300

    :fire:copyThumbNail manifestGenerator .json

--- py/src/main.py
+++ py/src/main.py
@@ -3,6 +3,9 @@ import sqlite3
 import zipfile
 import shutil
 from time import sleep
+from datetime import datetime, timedelta
+import pytz
+import json
 
 
 JWFILE1 = "files/bkp1.jwlibrary"
@@ -11,8 +14,10 @@ JWFILE2 = "files/bkp2.jwlibrary"
 
 os.makedirs("./data-3", exist_ok=True)
 
-for file in os.listdir("./data-3"):
-    os.remove(f"./data-3/{file}")
+
+def clearDir(dir):
+    for file in os.listdir(dir):
+        os.remove(f"{dir}/{file}")
 
 
 def readData1():
@@ -37,6 +42,27 @@ def copyAllFilesToData3():
             shutil.copy(f"./data-2/{file}", f"./data-3/{file}")
 
 
+def copyThumbNail():
+    shutil.copy("./extra/default_thumbnail.png",
+                "./data-3/default_thumbnail.png")
+
+
+def manifestGenerator():
+    now = datetime.now(pytz.timezone('Europe/Madrid'))
+    now_date = now.strftime("%Y-%m-%d")
+    hour_minute_second = now.strftime("%H-%M-%S")
+    now_iso = now.isoformat("T", "seconds")
+    now_utc = now.astimezone(pytz.UTC)
+    now_utc_iso = now_utc.isoformat("T", "seconds").replace('+00:00', 'Z')
+    schema_version = 14
+
+    j = '{{"name":"jwlibrary-plus-backup_{0}","creationDate":"{1}","version":1,"type":0,"userDataBackup":{{"lastModifiedDate":"{2}","deviceName":"jwlibrary-plus","databaseName":"userData.db","schemaVersion":{3}}}}}'.format(
+        now_date, now_date, now_iso, schema_version)
+    manifest = json.loads(j)
+
+    print(manifest)
+
+
 def db3():
     con = sqlite3.connect("./data-3/userData.db")
     cur = con.cursor()
@@ -44,16 +70,35 @@ def db3():
 
 
 if __name__ == "__main__":
-    print("Lendo bkp 1")
-    sleep(1)
+    print('>> Limpando pastas...')
+    clearDir("./data-1")
+    clearDir("./data-2")
+    clearDir("./data-3")
+
+    print(">> Lendo bkp 1")
+    sleep(.3)
     readData1()
 
-    print("Lendo bkp 2")
-    sleep(1)
+    print(">> Lendo bkp 2")
+    sleep(.3)
     readData2()
 
-    print("Copiando todos os arquivos para /data-3")
-    sleep(1)
+    print(">> Copiando todos os arquivos para /data-3")
+    sleep(.3)
     copyAllFilesToData3()
+
+    print(">> Copiando default_thumbnail.png para /data-3")
+    sleep(.3)
+    copyThumbNail()
+
+    print(">> Criando e copiando manifest.json para /data-3")
+    sleep(.3)
+    manifestGenerator()
+
+    # print('>> Limpando pastas...')
+    # sleep(.3)
+    # clearDir("./data-1")
+    # clearDir("./data-2")
+    # clearDir("./data-3")
 
     print('<<< FIM >>>')
