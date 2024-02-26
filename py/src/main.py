30b8cc4105f6062c8dbae2c2a867fed10341d87a
saulotarsobc <saulotarsobc@gmail.com>
2024-02-26 12:15:32 -0300

    :fire:get total count Tags table

--- py/src/main.py
+++ py/src/main.py
@@ -9,7 +9,25 @@ with zipfile.ZipFile(jwfile1, 'r') as zip_ref:
     files = zip_ref.namelist()
     zip_ref.extractall("./data-1")
 
-    uploadedDb = "files/bkp1.jwlibrary".format(
-        [zipname for zipname in files if zipname.endswith(".db")][0])
+    # uploadedDb = "./data-1/{0}".format([zipname for zipname in files if zipname.endswith(".db")][0])
+    uploadedDb = "./data-1/userData.db"
 
-    print(uploadedDb)
+    connection = sqlite3.connect(uploadedDb)
+    cursor = connection.cursor()
+    cursor.execute("SELECT Count(*) FROM Tag")
+    totoalTags = cursor.fetchall()[0][0]
+    print(f"Total de tags: {totoalTags}")
+
+# descompactar bkp2
+with zipfile.ZipFile(jwfile2, 'r') as zip_ref:
+    files = zip_ref.namelist()
+    zip_ref.extractall("./data-2")
+
+    # uploadedDb = "./data-2/{0}".format([zipname for zipname in files if zipname.endswith(".db")][0])
+    uploadedDb = "./data-2/userData.db"
+
+    connection = sqlite3.connect(uploadedDb)
+    cursor = connection.cursor()
+    cursor.execute("SELECT Count(*) FROM Tag")
+    totoalTags = cursor.fetchall()[0][0]
+    print(f"Total de tags: {totoalTags}")
